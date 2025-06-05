44b866ff35e29cf3ea604bd174e18ff61cd30ddc
kaluckii <kaluckii@proton.me>
2025-06-05 20:08:33 +0400

    Updated controllers export logic in importkit

--- itoolpack/importkit.py
+++ itoolpack/importkit.py
@@ -1,7 +1,9 @@
 import glob
 import importlib
+
 from types import ModuleType
-from typing import List, Optional
+
+from fastapi import APIRouter
 
 
 def escape_module(module: str) -> str:
@@ -13,11 +15,11 @@ def escape_module(module: str) -> str:
     return module.replace("\\", ".").replace("/", ".").replace(".py", "")
 
 
-def import_controllers(
-    path: Optional[str] = "controllers/*/**/controllers.py",
-) -> List[ModuleType]:
+def export_telegram_controllers(
+    path: str | None = "controllers/telegram/**/*.py",
+) -> list[ModuleType]:
     """
-    Imports all controllers modules dynamically, by default from the "controllers" directory.
+    Imports all controllers modules dynamically, by default from the "controllers/telegram" directory.
 
     :raises ModuleNotFoundError: If a module cannot be found during the import.
     """
@@ -30,7 +32,32 @@ def import_controllers(
     return modules
 
 
-def export_tortoise_models(path: Optional[str] = "domain/**/*/model.py") -> List[str]:
+def export_server_controllers(
+    path: str | None = "controllers/server/**/*.py",
+) -> list[APIRouter]:
+    """
+    Dynamically discovers and imports all Python modules matching the given glob path,
+    and collects any `controller` variables that are instances of FastAPI's `APIRouter`.
+
+    This is typically used to automatically register all route controllers from a specific
+    directory structure (e.g., `controllers/server/**`) without manually importing each one.
+
+    :param path: Glob pattern pointing to the target Python files (defaults to 'controllers/server/**/*.py').
+    :return: List of discovered APIRouter instances.
+    """
+    controllers = []
+
+    for m in glob.iglob(path, recursive=True):
+        module = importlib.import_module(escape_module(m))
+
+        if hasattr(module, "controller"):
+            if isinstance(module.controller, APIRouter):
+                controllers.append(module.controller)
+
+    return controllers
+
+
+def export_tortoise_models(path: str | None = "domain/**/*/model.py") -> list[str]:
     """
     Imports all models by searching recursively for `model.py` files in the
     specified directory structure and escaping their module paths. Returns the
