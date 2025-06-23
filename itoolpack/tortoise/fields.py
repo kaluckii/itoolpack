8fb7c31204102fdbb0e1f47d291968ebfc7516a4
kaluckii <kaluckii@proton.me>
2025-06-23 22:53:15 +0400

    Added tortoise functionality

--- itoolpack/tortoise/fields.py
+++ itoolpack/tortoise/fields.py
@@ -1,6 +1,12 @@
+from tortoise.models import Model as TortoiseModel
 from tortoise import fields
 
 
-def AutoNamedOneToOneField(model: str, *, to_model_name: str) -> fields.OneToOneField:
-    related_name = to_model_name.lower()
-    return fields.OneToOneField(model, related_name=related_name)
+class AutoRelatedNameModel(TortoiseModel):
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        for attr_name, attr_value in cls.__dict__.items():
+            if isinstance(attr_value, fields.relational.OneToOneFieldInstance):
+                if attr_value.related_name is None:
+                    attr_value.related_name = cls.__name__.lower()
