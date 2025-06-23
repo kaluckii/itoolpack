cdda30396e15922bd9ad0ae744f7975df97529d0
kaluckii <kaluckii@proton.me>
2025-06-23 22:58:55 +0400

    Updated tortoise model toolkit

--- itoolpack/tortoise/model.py
+++ itoolpack/tortoise/model.py
@@ -1,5 +1,6 @@
-from model import Model as __Model
-from model.contrib.pydantic import pydantic_model_creator
+from tortoise import fields
+from tortoise.models import Model as __Model
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class PydanticDescriptor:
@@ -12,3 +13,20 @@ class PydanticDescriptor:
 class Model(__Model):
     pd = PydanticDescriptor()
 
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.__assign_related_name()
+
+    @classmethod
+    def __assign_related_name(cls) -> None:
+        for attr_name, attr_value in cls.__dict__.items():
+            if isinstance(
+                attr_value,
+                (
+                    fields.relational.ForeignKeyFieldInstance,
+                    fields.relational.OneToOneFieldInstance,
+                    fields.relational.ManyToManyFieldInstance,
+                ),
+            ):
+                if attr_value.related_name is None:
+                    attr_value.related_name = cls.__name__.lower()
